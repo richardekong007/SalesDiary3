b9520bbe168384f3ef5446635fb6455ad94f7086
Richard Ekong <ekong.richard@yahoo.com>
2018-04-02 08:52:39 +0300

    Modified Login.java by referencing strings from the string.xml

--- app/src/main/java/com/example/adminstrator/salesdiary3/Login.java
+++ app/src/main/java/com/example/adminstrator/salesdiary3/Login.java
@@ -1,30 +1,19 @@
 package com.example.adminstrator.salesdiary3;
 
 /**
- * Created by Adminstrator on 6/16/2016.
+ * Created by Richard on 6/16/2016.
  */
 
 import android.app.AlertDialog;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-
-import java.util.Arrays;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.V;
 
 public class Login extends AppCompatActivity {
     private final String USERNAME = "USERNAME";
@@ -55,7 +44,7 @@ public class Login extends AppCompatActivity {
             final String PASSWORD = password.getText().toString().trim();
             salesRecordName = username.getText().toString().trim() + getString(R.string.sales_record_suffix);
 
-            if (!((uname.equals("")) || uname.length() <= 1) || (PASSWORD.equals("") || PASSWORD.length() <= 1))) {
+            if (!((uname.equals("")) || uname.length() <= 1) || (PASSWORD.equals("") || PASSWORD.length() <= 1)) {
                 if (db.getPassword(uname).equals(getString(R.string.invalid_user_message))) {
                     errorDialog = new AlertDialog.Builder(Login.this);
                     errorDialog.setTitle(getString(R.string.error_message));
@@ -108,34 +97,33 @@ public class Login extends AppCompatActivity {
                     errorDialog = new AlertDialog.Builder(Login.this);
                     errorDialog.setTitle(getString(R.string.error_dialog_title));
                     errorDialog.setIcon(R.mipmap.errorwhite);
-                    errorDialog.setMessage("Wrong password!");
-                    errorDialog.setPositiveButton("Close", null);
+                    errorDialog.setMessage(getString(R.string.error_password_message));
+                    errorDialog.setPositiveButton(getString(R.string.closebtn), null);
                     errorDialog.show();
                 }
             } else {
                 errorDialog = new AlertDialog.Builder(Login.this);
-                errorDialog.setTitle("ERROR MESSAGE");
+                errorDialog.setTitle(getString(R.string.error_dialog_title));
                 errorDialog.setIcon(R.mipmap.errorwhite);
-                errorDialog.setMessage("Invalid or no Username and Password specified");
-                errorDialog.setPositiveButton("Close", (dialog, id) -> dialog.dismiss());
+                errorDialog.setMessage(getString(R.string.error_login_message));
+                errorDialog.setPositiveButton(getString(R.string.closebtn), (dialog, id) -> dialog.dismiss());
                 errorDialog.show();
 
             }
         });
 
         signup.setOnClickListener((view) -> {
-
             confirmDialog = new AlertDialog.Builder(Login.this);
-            confirmDialog.setTitle("CONFIRMATION MESSAGE");
+            confirmDialog.setTitle(getString(R.string.confirm_dialog_title));
             confirmDialog.setIcon(R.mipmap.confirm2);
-            confirmDialog.setMessage("Are you sure?");
-            confirmDialog.setPositiveButton("Yes", (dialog, id) ->
+            confirmDialog.setMessage(getString(R.string.confirm_question_message));
+            confirmDialog.setPositiveButton(getString(R.string.yesbtn), (dialog, id) ->
             {
                 Intent gotoSignup = new Intent(Login.this, signUp.class);
                 clear(username, password);
                 startActivity(gotoSignup);
             });
-            confirmDialog.setNegativeButton("No", null);
+            confirmDialog.setNegativeButton(getString(R.string.nobtn), null);
             confirmDialog.show();
         });
 
---
+++ app/src/main/java/com/example/adminstrator/salesdiary3/ProductAdapter.java
@@ -0,0 +1,6 @@
+package com.example.adminstrator.salesdiary3;
+
+import android.support.v7.widget.RecyclerView;
+
+public class ProductAdapter extends RecyclerView {
+}
