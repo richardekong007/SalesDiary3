ec965788c80949bc0d9799a7a132ae81d7714893
Richard Ekong <ekong.richard@yahoo.com>
2018-04-02 08:54:28 +0300

    Refactored userProductCatalog to Product.java

--- app/src/main/java/com/example/adminstrator/salesdiary3/Product.java
+++ app/src/main/java/com/example/adminstrator/salesdiary3/Product.java
@@ -5,8 +5,7 @@ import java.io.Serializable;
 /**
  * Created by Adminstrator on 7/10/2016.
  */
-public class userProductCatalog implements Serializable
-{
+public class Product implements Serializable {
     //instance variable declaration
     private int Id;
     private String description;
@@ -15,65 +14,64 @@ public class userProductCatalog implements Serializable
     private Integer stock;
 
     //constructor
-    public userProductCatalog()
-    {
+    public Product() {
 
     }
-    public userProductCatalog(int Id, String description, Double costPrice, String imgPath, Integer stock)
-    {
-        this.Id=Id;
-        this.description=description;
-        this.costPrice=costPrice;
-        this.imgPath=imgPath;
-        this.stock=stock;
+
+    public Product(int Id, String description, Double costPrice, String imgPath, Integer stock) {
+        this.Id = Id;
+        this.description = description;
+        this.costPrice = costPrice;
+        this.imgPath = imgPath;
+        this.stock = stock;
     }
-    public userProductCatalog(String description, Double costPrice, String imgPath, Integer stock)
-    {
-        this.description=description;
-        this.costPrice=costPrice;
-        this.imgPath=imgPath;
-        this.stock=stock;
+
+    public Product(String description, Double costPrice, String imgPath, Integer stock) {
+        this.description = description;
+        this.costPrice = costPrice;
+        this.imgPath = imgPath;
+        this.stock = stock;
     }
+
     //create getter and setters
-    public int getId()
-    {
+    public int getId() {
         return this.Id;
     }
-    public void setId(int Id)
-    {
-        this.Id=Id;
+
+    public void setId(int Id) {
+        this.Id = Id;
     }
-    public String getDescription()
-    {
+
+    public String getDescription() {
         return this.description;
     }
-    public void setDescription(String productName)
-    {
-        productName=this.description;
+
+    public void setDescription(String productName) {
+        productName = this.description;
     }
-    public Double getCostPrice()
-    {
+
+    public Double getCostPrice() {
         return this.costPrice;
     }
-    public void setCostPrice(Double price)
-    {
-        this.costPrice=price;
+
+    public void setCostPrice(Double price) {
+        this.costPrice = price;
     }
-    public String getImagePath()
-    {
+
+    public String getImagePath() {
         return this.imgPath;
     }
-    public void setImagePath(String imgLocation)
-    {
-        this.imgPath=imgLocation;
+
+    public void setImagePath(String imgLocation) {
+        this.imgPath = imgLocation;
     }
-    public Integer getStock()
-    {
+
+    public Integer getStock() {
         return this.stock;
     }
-    public void setStock(int quantityLeft)
-    {
-        this.stock=quantityLeft;
+
+    public void setStock(int quantityLeft) {
+        this.stock = quantityLeft;
     }
 }
 
